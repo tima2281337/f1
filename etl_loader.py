9ab9fdf0c7bc1297fb69c13498085300e4d16554
Тамерлан Алиев <tamerlanaliev@MacBook-Air-Tamerlan.local>
2026-04-12 13:00:38 +0700

    modded: drivers parsing

--- etl_loader.py
+++ etl_loader.py
@@ -1,9 +1,20 @@
 import fastf1 as f1
 import pandas as pd
+import sqlite3 as sql
 
-session = f1.get_session(2021, 'French Grand Prix', 'Q')
+session = f1.get_session(2024, 7, 'Q')
 session.load(laps=False, telemetry=False)
 df = session.results[['FullName', 'TeamName', 'Abbreviation']]
 df = df.rename(columns={'FullName':'name', 'TeamName':'team', 'Abbreviation':'abb'})
+
+
+connection = sql.connect('f1max.db')
+try:
+    df.to_sql('drivers', connection, if_exists='append', index=False, method='multi')
+    print('добавили')
+except Exception as e:
+    print(f"что-то пошло не так: {e}")
+
+connection.close()
 
 print(df)
